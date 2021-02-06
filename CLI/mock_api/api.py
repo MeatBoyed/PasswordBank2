1d7f865bc5f194d36220d73fca2e4d061b4ee281
Charles <47355793+MeatBoyed@users.noreply.github.com>
2021-02-06 22:26:44 +0200

    Update api.py

--- CLI/mock_api/api.py
+++ CLI/mock_api/api.py
@@ -7,7 +7,7 @@ from .encryption import HashMasterPassword
 DB_HOST = os.environ.get('DB_HOST')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
-DB_DATABASE = "passwordbank2"
+DB_DATABASE = os.environ.get('DB_DATABASENAME')
 
 
 def ConnectToDatabase():
